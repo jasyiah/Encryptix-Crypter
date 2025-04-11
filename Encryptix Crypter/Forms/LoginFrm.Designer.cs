6cb86f21e99d01e16103e92b10162302aefd1295
Justifier0242 <fdgjhdsf3fd@outlook.com>
2025-04-11 17:40:20 +0400

    LoginFrm.Designer.cs

---
+++ Encryptix Crypter/Forms/LoginFrm.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace Crypter.Forms
+{
+    partial class loginFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(loginFrm));
+            this.guna2DragControl1 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.frmbackground = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.ShadowForm = new Guna.UI2.WinForms.Guna2ShadowForm(this.components);
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.status = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.frgtPswrd = new System.Windows.Forms.LinkLabel();
+            this.loginButton = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2ControlBox3 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2ControlBox5 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.password = new Guna.UI2.WinForms.Guna2TextBox();
+            this.username = new Guna.UI2.WinForms.Guna2TextBox();
+            this.OpacityTimer1 = new System.Windows.Forms.Timer(this.components);
+            this.guna2ControlBox2 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            ((System.ComponentModel.ISupportInitialize)(this.frmbackground)).BeginInit();
+            this.guna2Panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2DragControl1
+            // 
+            this.guna2DragControl1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2DragControl1.TargetControl = this.frmbackground;
+            this.guna2DragControl1.UseTransparentDrag = true;
+            // 
+            // frmbackground
+            // 
+            this.frmbackground.Image = ((System.Drawing.Image)(resources.GetObject("frmbackground.Image")));
+            this.frmbackground.ImageRotate = 0F;
+            this.frmbackground.Location = new System.Drawing.Point(3, -21);
+            this.frmbackground.Name = "frmbackground";
+            this.frmbackground.Size = new System.Drawing.Size(695, 471);
+            this.frmbackground.TabIndex = 3;
+            this.frmbackground.TabStop = false;
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BackColor = System.Drawing.Color.Black;
+            this.guna2Panel1.Controls.Add(this.status);
+            this.guna2Panel1.Controls.Add(this.label2);
+            this.guna2Panel1.Controls.Add(this.frgtPswrd);
+            this.guna2Panel1.Controls.Add(this.loginButton);
+            this.guna2Panel1.Controls.Add(this.guna2ControlBox3);
+            this.guna2Panel1.Controls.Add(this.guna2ControlBox5);
+            this.guna2Panel1.Controls.Add(this.password);
+            this.guna2Panel1.Controls.Add(this.username);
+            this.guna2Panel1.Controls.Add(this.frmbackground);
+            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(698, 450);
+            this.guna2Panel1.TabIndex = 0;
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.status.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.status.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.status.Location = new System.Drawing.Point(0, 431);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(207, 19);
+            this.status.TabIndex = 50;
+            this.status.Text = "Status: Awaiting login";
+            this.status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(237, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(214, 24);
+            this.label2.TabIndex = 49;
+            this.label2.Text = "Encryptix Crypter";
+            // 
+            // frgtPswrd
+            // 
+            this.frgtPswrd.ActiveLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.frgtPswrd.AutoSize = true;
+            this.frgtPswrd.BackColor = System.Drawing.Color.Transparent;
+            this.frgtPswrd.LinkColor = System.Drawing.Color.Gray;
+            this.frgtPswrd.Location = new System.Drawing.Point(301, 226);
+            this.frgtPswrd.Name = "frgtPswrd";
+            this.frgtPswrd.Size = new System.Drawing.Size(86, 13);
+            this.frgtPswrd.TabIndex = 48;
+            this.frgtPswrd.TabStop = true;
+            this.frgtPswrd.Text = "Forgot Password";
+            this.frgtPswrd.VisitedLinkColor = System.Drawing.Color.Lime;
+            this.frgtPswrd.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.frgtPswrd_LinkClicked);
+            // 
+            // loginButton
+            // 
+            this.loginButton.AutoRoundedCorners = true;
+            this.loginButton.BorderRadius = 21;
+            this.loginButton.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.loginButton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.loginButton.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.loginButton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.loginButton.FillColor = System.Drawing.Color.Red;
+            this.loginButton.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.loginButton.ForeColor = System.Drawing.Color.White;
+            this.loginButton.Location = new System.Drawing.Point(250, 252);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(180, 45);
+            this.loginButton.TabIndex = 46;
+            this.loginButton.Text = "Login";
+            this.loginButton.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2ControlBox3
+            // 
+            this.guna2ControlBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox3.BackColor = System.Drawing.Color.Black;
+            this.guna2ControlBox3.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
+            this.guna2ControlBox3.FillColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox3.HoverState.FillColor = System.Drawing.Color.CornflowerBlue;
+            this.guna2ControlBox3.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox3.Location = new System.Drawing.Point(605, 0);
+            this.guna2ControlBox3.Name = "guna2ControlBox3";
+            this.guna2ControlBox3.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox3.TabIndex = 45;
+            // 
+            // guna2ControlBox5
+            // 
+            this.guna2ControlBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox5.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox5.BorderColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox5.FillColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox5.ForeColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox5.HoverState.FillColor = System.Drawing.Color.Red;
+            this.guna2ControlBox5.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox5.Location = new System.Drawing.Point(653, 0);
+            this.guna2ControlBox5.Name = "guna2ControlBox5";
+            this.guna2ControlBox5.PressedColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox5.ShadowDecoration.Color = System.Drawing.Color.Red;
+            this.guna2ControlBox5.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox5.TabIndex = 42;
+            // 
+            // password
+            // 
+            this.password.AutoRoundedCorners = true;
+            this.password.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.password.BorderRadius = 17;
+            this.password.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.password.DefaultText = "";
+            this.password.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.password.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.password.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.password.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.password.FillColor = System.Drawing.Color.Black;
+            this.password.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.password.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.password.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.password.Location = new System.Drawing.Point(233, 182);
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.PlaceholderText = "Password";
+            this.password.SelectedText = "";
+            this.password.Size = new System.Drawing.Size(225, 36);
+            this.password.TabIndex = 2;
+            // 
+            // username
+            // 
+            this.username.AutoRoundedCorners = true;
+            this.username.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.username.BorderRadius = 17;
+            this.username.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.username.DefaultText = "";
+            this.username.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.username.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.username.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.username.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.username.FillColor = System.Drawing.Color.Black;
+            this.username.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.username.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.username.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.username.Location = new System.Drawing.Point(233, 140);
+            this.username.Name = "username";
+            this.username.PasswordChar = '\0';
+            this.username.PlaceholderText = "Username";
+            this.username.SelectedText = "";
+            this.username.Size = new System.Drawing.Size(225, 36);
+            this.username.TabIndex = 1;
+            // 
+            // OpacityTimer1
+            // 
+            this.OpacityTimer1.Tick += new System.EventHandler(this.OpacityTimer1_Tick);
+            // 
+            // guna2ControlBox2
+            // 
+            this.guna2ControlBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox2.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MaximizeBox;
+            this.guna2ControlBox2.FillColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox2.HoverState.FillColor = System.Drawing.Color.Silver;
+            this.guna2ControlBox2.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox2.Location = new System.Drawing.Point(608, 3);
+            this.guna2ControlBox2.Name = "guna2ControlBox2";
+            this.guna2ControlBox2.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox2.TabIndex = 44;
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox1.HoverState.FillColor = System.Drawing.Color.Red;
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(653, 3);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.ShadowDecoration.Color = System.Drawing.Color.Red;
+            this.guna2ControlBox1.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox1.TabIndex = 43;
+            // 
+            // loginFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 450);
+            this.Controls.Add(this.guna2Panel1);
+            this.Controls.Add(this.guna2ControlBox2);
+            this.Controls.Add(this.guna2ControlBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "loginFrm";
+            this.Text = "LoginFrm";
+            this.Load += new System.EventHandler(this.loginFrm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.frmbackground)).EndInit();
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2DragControl guna2DragControl1;
+        private Guna.UI2.WinForms.Guna2ShadowForm ShadowForm;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private System.Windows.Forms.Timer OpacityTimer1;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox3;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox2;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+        private Guna.UI2.WinForms.Guna2PictureBox frmbackground;
+        private Guna.UI2.WinForms.Guna2TextBox password;
+        private Guna.UI2.WinForms.Guna2TextBox username;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox5;
+        private Guna.UI2.WinForms.Guna2Button loginButton;
+        private System.Windows.Forms.LinkLabel frgtPswrd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label status;
+    }
+}
