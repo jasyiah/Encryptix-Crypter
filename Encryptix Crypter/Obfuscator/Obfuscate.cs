988dc4bda2acd1951d27271a6de136a6a3794d1a
Justifier0242 <fdgjhdsf3fd@outlook.com>
2025-04-11 17:40:35 +0400

    Obfuscate.cs

---
+++ Encryptix Crypter/Obfuscator/Obfuscate.cs
@@ -0,0 +1,19 @@
+﻿using Crypter.Obfuscator.Class;
+using dnlib.DotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypter.Obfuscator
+{
+    internal class Obfuscate
+    {
+        public void Execute(ModuleDefMD module)
+        {
+            Randomization.Execute(module);
+            Booster.Execute(module);
+        }
+    }
+}
