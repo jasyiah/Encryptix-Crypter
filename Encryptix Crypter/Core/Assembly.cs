c06fa0fba1872a0ef5d44e55bedb3ec8374c5ea6
Justifier0242 <fdgjhdsf3fd@outlook.com>
2025-04-11 17:39:57 +0400

    Assembly.cs

---
+++ Encryptix Crypter/Core/Assembly.cs
@@ -0,0 +1,21 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypter.Core
+{
+    public struct Assembly
+    {
+        public string iconPath;
+        public string assemblyTitle;
+        public string assemblyDescription;
+        public string assemblyProductName;
+        public string assemblyCompany;
+        public string assemblyVersion;
+        public string assemblyCopyright;
+        public bool usingAssembly;
+    }
+}
+*/
