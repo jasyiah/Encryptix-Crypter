456650189eed87544829f4db171166b66a53c3df
Justifier0242 <fdgjhdsf3fd@outlook.com>
2025-04-11 17:40:43 +0400

    AssemblyInfo.cs

---
+++ Encryptix Crypter/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Encryptix Crypter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Encryptix by unknown")]
+[assembly: AssemblyProduct("Encryptix Crypter")]
+[assembly: AssemblyCopyright("Copyright © Encryptix 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("fb235e27-38e1-432a-b405-b5872aafb4cd")]
+[assembly: AssemblyVersion("3.4.0")]
+[assembly: AssemblyFileVersion("3.4.0")]
